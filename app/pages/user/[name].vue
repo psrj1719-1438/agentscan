22339dfc4cc1f5c998beb49d02923562b42e8729
MatteoGabriele <m.gabriele.dev@gmail.com>
2026-03-07 20:32:57 +0100

    feat: better mobile card

--- app/pages/user/[name].vue
+++ app/pages/user/[name].vue
@@ -193,36 +193,32 @@ useHead({
     <!-- User Card -->
     <div
       v-if="user"
-      class="flex flex-col @lg:flex-row justify-center items-center @lg:items-start gap-6 bg-gh-card p-6 rounded-2 border-1 border-solid border-gh-border"
+      class="flex @lg:items-center gap-4 @lg:gap-6 bg-gh-card p-4 @md:p-6 rounded-2 border-1 border-solid border-gh-border"
     >
-      <div class="size-40 @lg:size-20 rounded-full bg-gray-500 shrink-0">
-        <img
-          v-if="user.avatar_url"
-          :src="user.avatar_url"
-          :alt="`Avatar of ${user.login}`"
-          class="size-40 @lg:size-20 rounded-full bg-gh-card"
-        />
+      <div
+        v-if="user.avatar_url"
+        class="size-12 @lg:size-20 rounded-full overflow-hidden bg-gh-card shrink-0"
+      >
+        <img :src="user.avatar_url" :alt="`Avatar of ${user.login}`" />
       </div>
 
-      <div
-        class="w-full flex flex-col justify-center items-center @lg:items-start text-center @lg:text-left"
-      >
-        <h2 class="text-gh-text text-3xl @lg:text-xl font-mono">
+      <div class="w-full flex flex-col">
+        <h2 class="text-gh-text text-xl font-mono">
           {{ user.name || user.login }}
         </h2>
         <NuxtLink
           :external="true"
           target="_blank"
           :to="`https://github.com/${user.login}`"
-          class="text-gh-muted underline text-xl @lg:text-sm"
+          class="text-gh-muted underline text-sm"
         >
           @{{ user.login }}
         </NuxtLink>
-        <p v-if="user.bio" class="my-2">
+        <p v-if="user.bio" class="my-2 text-sm @md:text-base">
           {{ user.bio }}
         </p>
         <ul
-          class="text-gh-muted mt-4 text-sm flex gap-2 sm:gap-4 flex-col items-center sm:flex-row @lg:items-start"
+          class="text-gh-muted @md:mt-4 text-sm hidden @xl:flex flex-wrap gap-4 flex-row"
         >
           <li class="flex items-center gap-1">
             <span
@@ -244,7 +240,7 @@ useHead({
               class="i-carbon-calendar hidden @md:flex shrink-0"
               aria-hidden="true"
             />
-            Member since
+            Since
             <NuxtTime :datetime="user.created_at" date-style="medium" />
           </li>
         </ul>
