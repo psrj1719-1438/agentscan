af484d76e0d4a8ea259a224a5aa1a28ed5223aee
MatteoGabriele <m.gabriele.dev@gmail.com>
2026-03-12 00:04:59 +0100

    test: fix time change

--- test/verified-automations.test.ts
+++ test/verified-automations.test.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it } from "vitest";
+import { expect, describe, it, beforeEach, afterEach, vi } from "vitest";
 import { identifyReplicant } from "../shared/utils/voight-kampff-test/identify-replicant";
 import fs from "fs";
 import path from "path";
@@ -19,7 +19,17 @@ function getFolder(folderName: "automation" | "organic") {
 
 const automationFixtures = getFolder("automation");
 describe("identity-replicant (automation)", () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
   it.each(automationFixtures)("analysis $1", (fixture) => {
+    const date = new Date(2026, 2, 10, 12);
+    vi.setSystemTime(date);
+
     const identity = identifyReplicant({
       createdAt: fixture.user.created_at,
       reposCount: fixture.user.public_repos,
@@ -32,7 +42,18 @@ describe("identity-replicant (automation)", () => {
 
 const organicFixtures = getFolder("organic");
 describe("identity-replicant (organic)", () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
   it.each(organicFixtures)("analysis $1", (fixture) => {
+    const date = new Date(2026, 2, 10, 12);
+    vi.setSystemTime(date);
+
+    console.log(new Date());
     const identity = identifyReplicant({
       createdAt: fixture.user.created_at,
       reposCount: fixture.user.public_repos,
