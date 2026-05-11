2aa324bf736a2c1c4a48ade02b73833d85d403f3
MatteoGabriele <m.gabriele.dev@gmail.com>
2026-05-11 18:47:22 +0200

    feat: add heatmap page

---
+++ app/pages/heatmap.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+});
+</script>
+
+<template>
+  <div class="h-screen w-screen flex items-center justify-center text-3xl">
+    🔥
+  </div>
+</template>
